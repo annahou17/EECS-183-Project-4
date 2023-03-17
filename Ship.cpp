8a0fd1e3a5fbf10d04498a878d0045735150435b
annahou17 <113255137+annahou17@users.noreply.github.com>
2023-03-16 21:19:47 -0400

    Update Ship.cpp

--- Ship.cpp
+++ Ship.cpp
@@ -4,18 +4,17 @@
  * Ship.cpp
  * Project UID 8885f2d9f10d2f99bc099aa9c3fc0543
  *
- * <#Name#>
- * <#Uniqname#>
+ * Anna Hou, Durrah  Azdi
+ * annahou, durrah
  *
  * Project 4: Battleship
  *
- * <#description#>
+ * Ship holds all the functions for Ship,h. Ship represents a ship in the Battleship game.
  */
 
 #include <cmath>
 
 #include "Ship.h"
-
 
 Ship::Ship() {
     start.set_row(0);
@@ -31,8 +30,10 @@ Ship::Ship(Position start_in, Position end_in) {
     end = end_in;
     num_hits = 0;
     if (is_horizontal()) {
+        //gets the start and end of the ships and subtracts them. then takes the absolute value
         size = abs(start.get_col() - end.get_col()) + 1;
     }
+    //if ship is vertical
     else {
         size = abs(start.get_row() - end.get_row()) + 1;
     }
@@ -51,6 +52,7 @@ int Ship::get_size() {
 }
 
 bool Ship::is_horizontal() {
+    //checks to make sure the start and end are in the same row
     if (start.get_row() == end.get_row()) {
         return true;
     }
@@ -61,27 +63,21 @@ bool Ship::is_horizontal() {
 
 bool Ship::has_position(Position pos) {
     if (is_horizontal()) {
+        //checks if the pos is in the same row as the starting value
         if ((pos.get_row() == start.get_row() &&
+             //checks if the pos is in between the start and end col values
             pos.get_col() >= start.get_col() &&
             pos.get_col() <= end.get_col())) {
             return true;
         }
-        if ((pos.get_row() == end.get_row() &&
-            pos.get_col() >= end.get_col() &&
-            pos.get_col() <= start.get_col())) {
-            return true;
-        }
     }
-    else { //ship is vertical
+    //ship is vertical
+    else {
+        //checks if the pos is in the same col as the starting value
         if ((pos.get_col() == start.get_col() &&
+             //checks if the pos is in between the start and end row values
             pos.get_row() >= start.get_row() &&
             pos.get_row() <= end.get_row())) {
-            return true;
-
-        }
-        if ((pos.get_col() == end.get_col() &&
-            pos.get_row() >= end.get_row() &&
-            pos.get_row() <= start.get_row())) {
             return true;
         }
     }
@@ -90,6 +86,8 @@ bool Ship::has_position(Position pos) {
 }
 
 void Ship::hit() {
+    /*checks the number of hits and makes sure it is less than size. if so
+    increase num_hits*/
     if (num_hits < size) {
         num_hits++;
     }
